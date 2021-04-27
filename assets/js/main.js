45c72758ba4fa22de184d39707c39315807da702
Vituz <vtbattaglia@gmail.com>
2021-04-27 16:56:21 +0200

    Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero 
+// e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 var counter = 0;
 
@@ -17,31 +20,14 @@ for (var i = 0; i < 100; i++){
     multi_5 = number%5;
 
     if (multi_3 == 0 && multi_5 == 0){
-        number += " FizzBuzz";
+        number = "FizzBuzz";
 
     } else if (multi_3 == 0) {
-        number += " Fizz";
+        number = "Fizz";
 
     } else if (multi_5 == 0){
-        number += " Buzz";
+        number = "Buzz";
     }
 
     console.log(number);
 }
-
-// outputText = '';
-// if (i % 3 == 0) {
-//     outputText += 'Fizz';
-// }
-// console.log(outputText)
-
-// var x = 100;
-// var y = 6;
-
-// var number = x%5;
-
-// console.log(number);
-
-// ma per i multipli di 3 stampi “Fizz” al posto del numero 
-// e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
